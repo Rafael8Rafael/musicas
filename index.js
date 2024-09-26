29bbc8626f7d7580a3be182b9e53e89849e77cd2
Rafael8Rafael <Raphaelpaula2025@icloud.com>
2024-09-26 20:26:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,1167 @@
+
+//ja bem vindo " +nome)/alert("se
+
+//alert = pormpt ("voce sabe que esta en lirk de risco")
+
+//var numero1 = prompt ("Digite o primeiro numero")
+//var numero2 = prompt ("Digite segundo numero")
+//alert(numero1 / numero2)
+
+
+//var numero1 = Number(prompt ("Digite o primeiro numero"))
+//var numero2 = Number(prompt ("Digite segundo numero"))
+
+//alert("o resultado da soma dos dois è:" + (numero1 + numero2))
+//alert("o resultado da soma dos dois è:" + (numero1 - numero2))
+//alert("o resultado da soma dos dois è:" + (numero1 * numero2))
+//alert("o resultado da soma dos dois è:" + (numero1 / numero2))
+
+//alert( +" o dobro de " + numero1 + " é "+ numero1 * 2 + "o triplo de " + numero1 + " è " + numero1 * 3)
+//alert("o resultado da soma dos dois è:" + (numero1 / numero2)) 
+
+//var nome = prompt ("qual o nome do aluno? ")
+// var numero1 = Number (prompt ("qual a primeira nota do" + nome ))
+//var numero2 = Number (prompt (" ok. primeira nota 10. qual segunda nota?"))
+//alert(" a medida final será " + (numero1 + numero2) / 2)
+//var media = ((nota1 + nota2 ) / 2)
+
+
+//if (MEDIA >= 6){
+///alert (" aluno " + nome + "apovado)
+//}
+//{else
+//   alert (" aluno " + nome + " reprovado")
+//}
+//perguntar a idade e salvar em uma variavavel
+
+//se a idade for maior ou igual a 18 
+//aparecer um alerta falando "maior de idade"
+//senao aparecer "menor de idade"
+
+
+//var numero1 = 100
+//var numero2 = 50
+//var numero3 = 3
+
+//alert(numerot2 numerot1)
+
+/*
+ var nota = pormpt ("qual e a nota do aluno")
+
+   if (nota >= 60){
+  alert ("aluno " + nota + "apovado")
+}
+else {
+
+    alert (" aluno " + nota + " reprovado")
+}
+*/
+/*
+var altura = pormpt (" qual e a altura aluno")
+var peso = prompt (" qual e o peso do aluno")
+var imc = peso/(altura * altura)
+  if ( altura >= 1,80)
+   alert ("altura " + peso + )
+
+
+
+  var peso = prompt ("qual a peso")
+  var altula = prompt ("qual a altula")
+
+  alert("seja bem vindo " +nome)
+
+  alert(imc)
+
+  if (imc <16.5){
+       alert("muto abaixo do peso")
+  }
+
+  if (imc < 17.85 && imc <= 18.4){
+     alert(" abaixo do peso")
+  }
+
+  if (imc <18.5  && imc <= 24.9){
+     alert("peso normal")
+  }
+  if (imc <25  && imc <= 29.9){
+     alert("acima do peso")
+  }
+  if (imc <30  && imc <= 34.9){
+     alert("obesidade grau1")
+  }
+  if (imc <35  && imc <= 40){
+     alert("obesidade grau2")
+  }
+   
+ */
+
+// var nomes=["alex" ,"joao" , "paulo"];
+
+
+//var lisa=[10 , 14,20,9,16,22];
+
+//var listadecursos = ["programaçao", "enfermagem","estetica","seguançao","informatica","excel","direito","administacao"]
+/*
+alert(listadecursos[0])
+alert(listadecursos[1])
+alert(listadecursos[2])
+alert(listadecursos[3])
+alert(listadecursos[4])
+alert(listadecursos[5])
+alert(listadecursos[6])
+alert(listadecursos[7])
+*/
+//listadecursos.forEach|(curso => alert(curso))
+/*
+for(var i=0; i<=6; i++){
+    alert(listadecursos[i])
+}
+for(var i=90; i<=0; i--){
+   alert(i)
+}
+   |*/
+/*   
+ var Number = (prompt(qual o valo da pimela nota))
+  var nota1 =namber(prompt("qual valor da nota 1"))
+  var nota = namber(prompt("qual o valor da nota 2"))
+  */
+
+/*
+     var qtdenota = prompt("quantas notas serao avaliadas")
+
+     var acumuladornotas = 0;
+
+
+
+     for (var i = 1<= qtdenotas; i++)
+      var nota = prompt("digite a nota" + 1)
+     var nota = prompt("digite a nota" + 2)
+     acumuladornotas = acumuladornotas +  Number(nota)
+
+     alert(" A media è: " + (acumuladornotas / Number(qtdenotas)))
+     listadenome.forEach( nome => alert(nome + "faz o curso de programaçao?"))
+alert(10=>10
+var numero1 = prompt ("Digite o primeiro numero1)
+*/
+
+
+
+
+//var llistaDeprosença =[" Maylon", "Arthur", "Ryan", "raphael", "gabiel", "maria", "macoly" , "dalton", "patrick"]
+//var indicenome = prompt("qualo o indice do alumo")
+//alert(listadenome [indiceprocurado])
+//var indiceprocurado = prompt("digite o indice do aluno que deseja buscar")
+//alert(listadepresença[indiceprocurado])
+/*
+var numero = prompt("digite um numelo")
+
+alert(numero * 8 +)
+*//*
+var numero = prompt("qual o numero")
+ for(var i=0; i<=9; i++){
+   alert(i + " x " + numero + "=" + (i * numero))  
+
+
+
+
+ }
+*/
+/*
+
+ var listadejogo = ["fifa", "call of duty", "red dead" , "cs"]
+
+listadajogo.push(" the witcher")
+
+alert(listadajogo[4])
+*/
+/*&
+listadejogo.pop()//REMOVE O ULTIMO ELEMETO DA LISTA
+listadejogo.shift()//remove O PRIMEIRO ELEMENTO
+listadejogo.splice(2, 1)//REMOVE 1 ELEMENTO A PARTIR DO INDICE 2
+*/
+//alert(listadejogo)
+
+//var indice = listadejogo.idexOF("fifa")
+//alert( indice )
+
+//listadejogo.forEach(jogo => alert("estou jogando" + jogo))
+
+//var listadejogo = ["fifa", "call of duty", "red dead" , "cs"]
+//var qtdejogosnovos = prompt("quantos jogos vc quer adicionar")
+//for(var i=0; i<qtdejogosnovos; i++){
+//listadejogo.push(jogo)
+//}
+
+//alert(listadejogo)
+
+/*
+var operacao = prompt("1-saldo\n2- transferencia\n3- sair do sistema")
+var i = 0;
+while(i <10){
+     alert(i)
+       i+++
+}*/
+/*
+ alert("ola seja bem vido no sistema")
+
+
+var operacao = prompt("1-saldo\n2- transferencia\n3- sair do sistema")
+
+var saldo = 10000
+
+if (operacao==1){
+   alert("o seu saldo è:" + saldo)
+}
+   if (operacao==2){
+var quatia = prompt("qual a quantia que voce quer tira")
+alert( saldo - quantia)
+}   
+*/
+/*
+ Em uma lista, para que serve o ForEac
+
+var curso = {
+   nome: "losica de progamaçao",
+ alumos: listadeprosença,
+ cargaHoraria:60,
+ materia:"javascript",
+ datainicio: "27/08/2024",
+datafinal:"29/09/2024",
+}
+alert(curso.nome + "e um curso com" + curso.cargaHoraria + "h de carga horaria")
+*/
+//C compraralmoco
+//alert("compra uma lasanha")
+/*
+ function  rotinadiaria(){
+   alert(" acordar")
+   alert("arrumar a cama")
+   alert("ir trabalhar")
+ *//*
+     function somarnumeros (num1, num2, num3){
+        var num1 = number(prompt("digite o primeiro numero"))
+        var num2 = number(prompt("digite o segundo numero "))
+        var num3 = number(prompt("digite o erceiro numero"))
+        var resultado = num1 * num2 * num3
+        alert(resultado)
+     }    
+   */
+/*
+   function classificador (){
+      var numero = prompt("qual o numero")
+      if (numero>10){
+         alert(numero +"mario do que dez")
+      }
+      else if ( numero==10){
+         alert(numero + " e igual aq dez")
+
+          
+      else {
+         alert(numero + "menor que dez")
+      }
+ }
+
+  classificados() 
+*/
+/*
+var cep = prompt("qual o cep")
+var cepformatado = cep.substring(0,5) + "-" + ccep.substring(0,5) + "-" + cep.substring(5,8))
+alert(cep.substring(0,5) + "-" + cep.substring(5,8))
+
+alert(cepformatado)
+*/
+//var CPF ="023.122.676-40";
+//var CPF = CPF.substring(0,3) + "." + CPF.substring(3,
+// alert(testaCPF(steCPF));
+
+
+
+// Objeto JavaScrip//
+
+
+var nome = "Mavi"
+alert("Ola seja bem vindo ao sistema " + nome)
+
+var musicas = [{nome: "Lets go 4" , cantor:"MC ryan sp" , datalanc:2023 }  , {nome:"voltei pras putas" , cantor:"MC gp" , datalanc:2019 } , {nome:"sagrado profano" , cantor:"luisa sonza" , datalanc:2023} , {nome:"casei com a putaria" , cantor:" MC paiva zs" , datalanc:2022} , {nome:"pensa que e dois" , cantor:"MC pipokinha" , datalanc:2024} , {nome:"sorriso no rosto" , cantor:" MC ig" , datalanc:2024} ,  {nome:"vai la em casa hoje", cantor:"Marilia mendonça", datalanc:2021} , {nome:"solteiro forçado" , cantor:"Ana castela" , datalanc:2024}]
+var listademusicas = " "
+musicas.forEach(musicas => listademusicas = listademusicas + musicas.nome + "[" + musicas.datalanc + "]" + "[" + musicas.cantor + "]" + "\n" ) 
+
+var cantores = [{nome:"Henrique e Juliano"} , {nome:"Gustavo Lima"} , {nome:"Ze neto e Cristiano"} , {nome:"Jorge e Mateus" } , {nome:"Wesly Safadão"} , {nome:"Mateus e Kauan"} , {nome:"Maiara e Maraisa"} , {nome:"Luan Santana"} , {nome:"Simone e Simaria"} ]
+var listamaisescutados = " "
+cantores.forEach(cantores => listamaisescutados = listamaisescutados + cantores.nome + "\n" ) 
+
+
+operacao = prompt("o que vc deseja fazer? \n1- Ver musicas disponiveis \n2- Cantores mais ouvidos \n3 Sair do saite ")
+
+if (operacao== 1){
+   alert("Musicas disponiveis " + listademusicas)
+} 
+
+if (operacao== 2){
+    alert("Cantores mais ouvidos " + listamaisescutados)
+}
+
+if (operacao== 3){
+   alert(prompt("Qual sua nota pro saite Mavi?"))
+   alert("Obrigado(a)!!!")
+}
+
+/*
+istademusica.forEach | (musica => alert(musica))
+
+for (var i = 0; i <= 6; i++) {
+   alert(listademusica[i])
+}
+for (var i = 90; i <= 0; i--) {
+   alert(i)
+   alert(" obrigada por usar o saite")
+}
+*/
+/*
+var mc = prompt ("Digite o primeiro mc")
+var numero2 = prompt ("Digite segundo numero")
+alert(numero1 / numero2)
+  /*     
+   alert("seja bem vindo " +nome)
+
+alert = pormpt ("voce sabe que esta en lirk de risco")     
+ 
+alert("seja bem vinda ou mavi")
+var operacao = prompt("o que vc deseja fazer? \n1- var musica disponiveis \n2- cadastra musica")
+if(operacao == 1)}
+alert("musica disponiveis")
+}
+else if (operacao == 2){
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
